539e69715f6717bec8e7b60693516153f492a1ea
Steffen <stew-1989@hotmail.com>
2016-10-20 22:17:20 +0200

    Made few changes to the data variable

--- base.r
+++ base.r
@@ -1,9 +1,10 @@
 require("RSQLite")
 
+# Create new columns, w/L/D Column, match id column
+
 main <- function() {
   sqlite <- dbDriver("SQLite")
   con <- dbConnect(sqlite, "data/database.sqlite")
-  dbListTables(con)
   matches <- dbGetQuery(con, "SELECT 
                        m.country_id, 
                        m.league_id, 
@@ -18,7 +19,23 @@ main <- function() {
                     team as t_away ON m.away_team_api_id = t_away.team_api_id
                   JOIN 
                     team as t_home ON m.home_team_api_id = t_home.team_api_id
-                  LIMIT 400")
+                  ")
   
-  print(matches)
+  return(matches)
+}
+
+data <- main()
+
+locate_NA <- function(x){
+  ind <- which(is.na(x))
+  return(ind)
+}
+
+w_l_d_column <- function(){
+  for(d in data){
+    
+  }
+  
+  data$W/L/D <- w_l_d_vector
+  
 }
