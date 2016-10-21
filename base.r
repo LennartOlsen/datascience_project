3b7c629fbb8789ec420aca01fff774eea7865523
SuperStewDK <stew-1989@hotmail.com>
2016-10-21 05:29:13 +0200

    Started working on wrangling data. Preparing to rank teams

--- base.r
+++ base.r
@@ -4,9 +4,9 @@ main <- function() {
   sqlite <- dbDriver("SQLite")
   con <- dbConnect(sqlite, "data/database.sqlite")
   matches <- dbGetQuery(con, "SELECT 
-                       m.country_id, 
                        m.league_id,
-                       m.date, 
+                       m.date,
+                       m.season,
                        m.away_team_goal, 
                        m.home_team_goal, 
                        t_away.team_long_name as away_team_name, 
@@ -48,3 +48,60 @@ data$w_l_d_away <- ifelse(data$home_team_goal == data$away_team_goal, 2,
 number_of_wins <- length(data$w_l_d_home[data$w_l_d_home == 0])
 number_of_losses <- length(data$w_l_d_home[data$w_l_d_home == 1])
 number_of_draws <- length(data$w_l_d_home[data$w_l_d_home == 2])
+
+# get individual team points in a season
+get_season_points <- function(home, away){
+  points = 0
+  for(m in home){
+    if(m == 0){
+      points = points + 3
+    } else if(m == 2){
+      points = points + 1
+    }
+  }
+  
+  for(m in away){
+    if(m == 0){
+      points = points + 3
+    } else if(m == 2){
+      points = points + 1
+    }
+  }
+  return(points)
+}
+
+# get individual team goal deficit in a season
+get_season_deficit <- function(home_team, away, home, away_team){
+  team_total <- sum(home_team + away_team)
+  opposing_team_total <- sum(away + home)
+  deficit <- team_total - opposing_team_total
+}
+
+#create a league rank function. 
+#League_id is an int 
+#season is characters "2008/2009" 
+#team is characters "Basel"
+team_rank_in_league <- function(seasonID, leagueID, teamName){
+  season_rank = 0
+  
+  # retrieves all data from a specific season
+  season_data <- data[data$season == seasonID & data$league_id == leagueID,]
+  
+  teams = c(season_data$home_team_name[!duplicated((season_data$home_team_name))])
+  num_of_teams = length(teams)
+  
+  league_ranking <- data.frame(rank= integer(num_of_teams), teams, points= integer(num_of_teams), deficit= integer(num_of_teams))
+  
+  for(team in teams){
+    team_data_away <- as.data.frame(season_data[season_data$away_team_name == team,])
+    team_data_home <- as.data.frame(season_data[season_data$home_team_name == team,])
+    
+    points <- get_season_points(team_data_home$w_l_d_home,team_data_away$w_l_d_away)
+    deficit <- get_season_deficit(team_data_home$home_team_goal,team_data_home$away_team_goal, 
+                                  team_data_away$away_team_goal,team_data_away$home_team_goal)
+    
+    league_ranking[league_ranking$teams == team,] <- c(0, team, points, deficit)
+  }
+  
+  return(season_rank)
+}
